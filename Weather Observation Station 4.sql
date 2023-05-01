5c9e80e4a196861ccc5f6a75c9eb7f8b19b21793
Kabi Raj <33712012+irkian@users.noreply.github.com>
2023-05-01 10:16:40 -0400

    Update and rename Weather Observation Station 4 to Weather Observation Station 4.sql

--- Weather Observation Station 4.sql
+++ Weather Observation Station 4.sql
@@ -1,4 +1,5 @@
+/*
 Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
-
 ANSWER:
+*/
 SELECT (COUNT(ID) - COUNT(DISTINCT CITY)) AS DIFF FROM STATION;
